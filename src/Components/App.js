65e3d7bccc35d8e17b5ab6e8c0bf5c4c2c660bb5
ingleonelrv <ing.leonelrv@gmail.com>
2019-06-20 12:41:41 -0600

    loading users from api with trae in App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
+import trae from "trae";
+import "./App.css";
 
-const App = () => {
-  return <div>Hola</div>;
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: false,
+      error: null
+    };
+  }
+  async componentDidMount() {
+    const response = await trae.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    this.setState({
+      users: response.data
+    });
+  }
+  putData = () =>
+    this.state.users.map(user => (
+      <tr key={user.id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.website}</td>
+      </tr>
+    ));
+  render() {
+    return (
+      <div className="margin">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <thead>{this.putData()}</thead>
+        </table>
+      </div>
+    );
+  }
+}
 export default App;
